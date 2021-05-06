6c8bcd3932093d3ab1e084727834d6d26fb76e4c
Deshawn Monzon <inthemonzon@yahoo.com>
2021-05-06 00:22:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyCustomHolidayTo(Birthday, Mom) {
   return `Happy ${Birthday}, ${Mom}!`
 }
 
-function holidayCountdown(Christmas, 20) {
-  return `It's ${20} days until ${Christmas}!`
+function holidayCountdown(Christmas, days) {
+  return `It's ${10} days until ${Christmas}!`
 }
